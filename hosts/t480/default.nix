5a99d0f80410e4a4c482bad5aebd58c7ff108633
Ashpex <ashpex@pm.me>
2026-02-23 22:58:54 +0700

    Extract hardware settings from hardware-configuration.nix to t480/default.nix

--- hosts/t480/default.nix
+++ hosts/t480/default.nix
@@ -1,9 +1,9 @@
 # Configuration for ThinkPad T480
-{ config, pkgs, ... }:
+{ config, lib, pkgs, modulesPath, ... }:
 
 {
   imports = [
-    # ./hardware-configuration.nix  # Commented out for VM testing
+    (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
@@ -13,6 +13,17 @@
 
   # Use latest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Hardware-specific settings (from hardware-configuration.nix)
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  # Intel CPU microcode updates
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Filesystems are managed by Disko (see configuration.nix)
 
   # ThinkPad-specific packages
   users.users.ashpex.packages = with pkgs; [
