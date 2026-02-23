d5f512c3725006e27bb3020a2600fb52e43e453d
Ashpex <ashpex@pm.me>
2026-02-23 23:13:22 +0700

    Re-enable HyprPanel and auto-start terminal for debugging

--- users/ashpex/hyprland.nix
+++ users/ashpex/hyprland.nix
@@ -4,31 +4,34 @@
 
 {
   home-manager.users.ashpex = {
-    # Enable HyprPanel (temporarily disabled to test home-manager)
-    # programs.hyprpanel = {
-    #   enable = true;
-    #
-    #   # Optionally configure HyprPanel settings
-    #   # See: https://hyprpanel.com/configuration/panel.html
-    #   # settings = {
-    #   #   bar.layouts = {
-    #   #     "0" = {
-    #   #       left = [ "dashboard" "workspaces" "windowtitle" ];
-    #   #       middle = [ "media" ];
-    #   #       right = [ "volume" "clock" "notifications" ];
-    #   #     };
-    #   #   };
-    #   #   theme = {
-    #   #     font = {
-    #   #       name = "JetBrainsMono Nerd Font";
-    #   #       size = "14px";
-    #   #     };
-    #   #   };
-    #   # };
-    # };
+    # Enable HyprPanel
+    programs.hyprpanel = {
+      enable = true;
+
+      # Optionally configure HyprPanel settings
+      # See: https://hyprpanel.com/configuration/panel.html
+      # settings = {
+      #   bar.layouts = {
+      #     "0" = {
+      #       left = [ "dashboard" "workspaces" "windowtitle" ];
+      #       middle = [ "media" ];
+      #       right = [ "volume" "clock" "notifications" ];
+      #     };
+      #   };
+      #   theme = {
+      #     font = {
+      #       name = "JetBrainsMono Nerd Font";
+      #       size = "14px";
+      #     };
+      #   };
+      # };
+    };
 
     # HyprPanel handles notifications, so other notification daemons should be disabled
     # services.dunst.enable = false;
     # services.mako.enable = false;
+
+    # Auto-start terminal for debugging in VM
+    wayland.windowManager.hyprland.settings.exec-once = [ "kitty" ];
   };
 }
