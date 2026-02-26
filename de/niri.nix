6b9b902e5274ff90d1040e26310c552d39431027
Ashpex <ashpex@pm.me>
2026-02-26 17:59:28 +0700

    fix: use uwsm start directly to avoid import-environment deprecation

--- de/niri.nix
+++ de/niri.nix
@@ -23,7 +23,7 @@
       enable = true;
       settings = {
         default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd niri-session";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd \"uwsm start niri.desktop\"";
           user = "greeter";
         };
       };
